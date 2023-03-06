5872965ad7ec6af521ebe2b61f72a2f7bbc57a9a
Mahdi <hassanim430@gmail.com>
2023-03-06 19:53:30 +0430

    adding routes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Connection to database
 const connectDB = require("./config/db");
 connectDB();
 
+// API integration
+app.use("/api/users", require("./routes/users"));
+app.use("/api/products", require("./routes/products"));
+
+// Listener to server port
 app.listen(PORT, () => {
   console.log(`Server is listening to port: ${PORT}`);
 });
