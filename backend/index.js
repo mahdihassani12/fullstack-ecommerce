7c5cd948d4dcf8119b45377fa570cf44686c9c79
Mahdi <hassanim430@gmail.com>
2023-03-06 19:39:08 +0430

    connect to mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-app.get("/", (req, res) => {
-  res.send("Some text from app");
-});
+const connectDB = require("./config/db");
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is listening to port: ${PORT}`);
