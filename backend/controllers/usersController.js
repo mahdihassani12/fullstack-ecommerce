7903d8ebda037f8d486d5487bc62ab3324a66c83
Mahdi <hassanim430@gmail.com>
2023-03-08 21:19:17 +0430

    Saving user to database

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 exports.getUsers = async (req, res, next) => {
   try {
@@ -10,15 +11,32 @@ exports.getUsers = async (req, res, next) => {
 };
 
 exports.postUsers = async (req, res, next) => {
-  try {
+  const { name, email, password, role } = req.body;
 
+  try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }else {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      let user = await User.findOne({ email });
 
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+        role,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      user.save();
     }
-
   } catch (error) {
     res.status(500).send("Server error ", error);
   }
