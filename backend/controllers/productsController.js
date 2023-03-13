2b4b89f99d959d0090ebd723b9a8499dd894e09b
Mahdi <hassanim430@gmail.com>
2023-03-13 20:58:51 +0430

    storing the product to database

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,9 +1,28 @@
 const { validationResult } = require("express-validator");
+const Product = require("../models/Product");
 
-exports.getProducts = async (req, res, next) => {
-
-}
+exports.getProducts = async (req, res, next) => {};
 
 exports.postStoreProducts = async (req, res, next) => {
-    
-}
+  const { name, description, category, price, brand, quantity } = req.body;
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      const newProduct = new Product({
+        userId: req.user.id,
+        name,
+        description,
+        category,
+        price,
+        brand,
+        quantity
+      });
+      const product = await newProduct.save();
+      res.json({ product });
+    }
+  } catch (error) {
+    res.status(500).send("Server error", error.message);
+  }
+};
